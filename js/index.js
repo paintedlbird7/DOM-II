82bede0c05e37fd27bb8c4c2383c521b53668327
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-05 14:33:11 -0800

    Completed MVP

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// // Your code goes here 	
+// Event Handler 
 
 //* [x] `mouseover`
 const imgChange = document.querySelector('img');
@@ -8,33 +8,73 @@ imgChange.addEventListener('mouseover', function(event) {
 
 
 // 	* [x] `keydown`
-const firstName = document.querySelector('.first-name');
 
-firstName.addEventListener('keydown', e => {
-  console.log(`This is my event: ${e.code}`);
-});
+const firstName = document.querySelector(".first-name");
+firstName.addEventListener('keydown', function(event) {
+  console.log(`this is the blank key: ${event.code}`)
+})
+
+// const firstName = document.querySelector('.first-name');
+
+// firstName.addEventListener('keydown', e => {
+//   console.log(`This is my event: ${e.code}`);
+// });
 
 
-// 	* [ ] `wheel`
+	// * [?] `wheel`
+const footerResize = document.querySelector('.footer')
+footerResize.addEventListener("wheel", function(event) {
+    console.log("All resources have wheeled!");
+  });
+
+
+// window.addEventListener("wheel", function(event) {
+//     console.log("All resources finished loading!");
+//   });
+
 // 	* [ ] `drag / drop`
-// 	* [ ] `load`
-// 	* [ ] `focus`
-// 	* [ ] `resize`
-// 	* [ ] `scroll`
 
+// 	* [x] `load`
+window.addEventListener("load", function(event) {
+    console.log("All resources finished loading!");
+  });
+
+
+    // * [x] `focus`
+    const emailAddress = document.querySelector('.email')
+
+    emailAddress.addEventListener("focus", function(event) {
+    console.log("All resources have focus!");
+      // stops the original behavior of HTML elements
+  });
+
+
+// 	* [x] `resize`
+// const footerResize = document.querySelector('.footer')
+// footerResize.addEventListener("resize", function(event) {
+//     console.log("All resources have resized!");
+//   });
+window.addEventListener("resize", function(event) {
+    console.log("All resources have resized!");
+  });
+
+
+// 	* [x] `scroll`
+window.addEventListener("scroll", function(event) {
+    console.log("All resources have scrolled!");
+  });
 
 
 
 // 	* [x] `select`
-const logoHeading = document.querySelector('.first-name')
-logoHeading.addEventListener('select', function(event) {
+const firstNameField = document.querySelector('.first-name')
+firstNameField.addEventListener('select', function(event) {
   console.log(`When text field is selected it fires a select event`)
   //event.stopPropagation();
 });
 
 
 // 	* [x] `dblclick`
-// Event Handler 
 const signBtn = document.querySelector('.btn')
 signBtn.addEventListener('dblclick', function(event) {
   console.log(`Sign up button fired a click event`)
@@ -43,12 +83,13 @@ signBtn.addEventListener('dblclick', function(event) {
 
 
 
+
+
 // Using the 10 unique events, find ways to update the DOM in creative ways. 
 //For example you could change colors, animate objects, remove objects, etc.
 
 // * [x] Nest two similar events somewhere in the site and prevent the event propagation 
 //properly
-
 const containerBtn = document.querySelector('.main-navigation')
 containerBtn.addEventListener('click', function(event) {
   
@@ -62,8 +103,15 @@ console.log(`this is my current target: ${event.currentTarget}`)
 })
 
 
-// * [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
 
+
+// * [x] Stop the navigation from items from refreshing the page by using `preventDefault()`
+const formSubmit = document.querySelector('.form-submit');
+formSubmit.addEventListener('click', e => {
+  console.log(`Work button fired a click event!`);
+  // stops the original behavior of HTML elements
+  e.preventDefault();
+});
 
 
 
