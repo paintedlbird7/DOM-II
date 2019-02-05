1b7d543101f548434fdf35809c2d06f1b53b7e49
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-05 11:32:28 -0800

    Working on Event Handlers within the DOM

--- js/index.js
+++ js/index.js
@@ -1,30 +1,55 @@
-// // Your code goes here 	* [ ] `mouseover`
-// 	* [ ] `keydown`
+// // Your code goes here 	
+
+//* [x] `mouseover`
+const imgChange = document.querySelector('img');
+imgChange.addEventListener('mouseover', function(event) {
+  event.target.style.border = '5px dashed teal'
+})
+
+
+// 	* [x] `keydown`
+const firstName = document.querySelector('.first-name');
+
+firstName.addEventListener('keydown', e => {
+  console.log(`This is my event: ${e.code}`);
+});
+
+
 // 	* [ ] `wheel`
 // 	* [ ] `drag / drop`
 // 	* [ ] `load`
 // 	* [ ] `focus`
 // 	* [ ] `resize`
 // 	* [ ] `scroll`
-// 	* [ ] `select`
 
 
-// 	* [ ] `dblclick`
 
+
+// 	* [x] `select`
+const logoHeading = document.querySelector('.first-name')
+logoHeading.addEventListener('select', function(event) {
+  console.log(`When text field is selected it fires a select event`)
+  //event.stopPropagation();
+});
+
+
+// 	* [x] `dblclick`
 // Event Handler 
-
 const signBtn = document.querySelector('.btn')
 signBtn.addEventListener('dblclick', function(event) {
   console.log(`Sign up button fired a click event`)
-  // event.stopPropagation();
+  event.stopPropagation();
 });
 
 
 
-// Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+// Using the 10 unique events, find ways to update the DOM in creative ways. 
+//For example you could change colors, animate objects, remove objects, etc.
 
-// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation 
+//properly
 // * [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+
 
 
 
