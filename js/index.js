e461caf8675096f28c6a9d1b1b401eaa26ccd21f
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-05 10:54:15 -0800

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-// Your code goes here
+// // Your code goes here 	* [ ] `mouseover`
+// 	* [ ] `keydown`
+// 	* [ ] `wheel`
+// 	* [ ] `drag / drop`
+// 	* [ ] `load`
+// 	* [ ] `focus`
+// 	* [ ] `resize`
+// 	* [ ] `scroll`
+// 	* [ ] `select`
+
+
+// 	* [ ] `dblclick`
+
+// Event Handler 
+
+const signBtn = document.querySelector('.btn')
+signBtn.addEventListener('dblclick', function(event) {
+  console.log(`Sign up button fired a click event`)
+  // event.stopPropagation();
+});
+
+
+
+// Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+// * [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+
+
+
